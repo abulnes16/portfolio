61b6e494bb007de7c095298dc3eb86147c45f565
Angel Bulnes <angel_bulnes16@yahoo.com>
2020-12-21 19:48:20 -0600

    Corregido menu

--- js/app.js
+++ js/app.js
@@ -2,9 +2,5 @@ const menuBtn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
 
 menuBtn.addEventListener("click", () => {
-  if (menu.classList.contains("is-active")) {
-    menu.classList.remove("is-active");
-  } else {
-    menu.classList.add("is-active");
-  }
+  menu.classList.toggle("is-active");
 });
