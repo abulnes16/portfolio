c597c48cd18c76af73d1bfcce22cabe29e04624e
Angel Bulnes <abulnes16@MacBook-Pro-de-Angel.local>
2025-05-28 11:39:51 -0600

    Add Google Analytics

--- src/App.js
+++ src/App.js
@@ -6,9 +6,18 @@ import Hero from "./components/hero/Hero";
 import Projects from "./components/projects/Projects";
 import Spinner from "./components/spinner/Spinner";
 import { useProjects } from "./hooks";
+import ReactGA from "react-ga4";
+import { useEffect } from "react";
+
+ReactGA.initialize("G-QQF4T48FJ8");
 
 function App() {
   const { loading, projects } = useProjects();
+
+  useEffect(() => {
+    // Enviar vista de página al cargar la aplicación
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
 
   return (
     <div className="App">
